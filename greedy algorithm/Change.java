5bbede8be4f9674338d8b82310b4749f5c36484f
Peterren <peterren0205@gmail.com>
2017-10-14 22:37:16 -0400

    Add files via upload

---
+++ greedy algorithm/Change.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Change {
+    private static int getChange(int m) {
+        int a = m/10;
+        int b = (m%10)/5;
+        int c = m - 10*a-5*b;
+        
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+        System.out.println(getChange(m));
+
+    }
+}
+
---
+++ greedy algorithm/DifferentSummands.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class DifferentSummands {
+    private static List<Integer> optimalSummands(int n) {
+        List<Integer> summands = new ArrayList<Integer>();
+        int k = n;
+        int l = 1;
+        while (k > 0) {
+            if (k <= 2*l) {
+                summands.add(k);
+                k -= k;
+            } 
+            else {
+                summands.add(l);
+                k -= l;
+                l += 1;
+            }
+
+        }
+
+
+        return summands;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        List<Integer> summands = optimalSummands(n);
+        System.out.println(summands.size());
+        for (Integer summand : summands) {
+            System.out.print(summand + " ");
+        }
+    }
+}
+
